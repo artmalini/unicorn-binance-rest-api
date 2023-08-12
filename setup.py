30885a5f34f474fa67d76d4db282845134ac72a1
Artem <artmalini@gmail.com>
2023-08-12 12:05:54 +0300

    new fapi for futures fix

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setuptools.setup(
      version=str(ubra.get_version()),
      author="LUCIT Systems and Development",
      author_email='info@lucit.tech',
-     url="https://github.com/LUCIT-Systems-and-Development/unicorn-binance-rest-api",
+     url="https://github.com/artmalini/unicorn-binance-rest-api",
      description="An unofficial Python API to use the Binance REST API`s (com+testnet, com-margin+testnet, "
                  "com-isolated_margin+testnet, com-futures+testnet, us, tr) in a easy, fast, flexible, robust and "
                  "fully-featured way. ",
